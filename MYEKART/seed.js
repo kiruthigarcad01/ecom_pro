4d8956fcbccd2a6cd8fd140814755f0fe000ee88
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2025-01-10 11:00:52 +0530

    seed and admin files logics modified

--- MYEKART/seed.js
+++ MYEKART/seed.js
@@ -1,21 +1,41 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken'); 
+
+
+const JWT_SECRET = process.env.JWT_SECRET; 
+
+const generateToken = (adminId, email) => {
+  return jwt.sign(
+    { 
+      id: adminId,
+      email: email,
+      role: 'admin'
+    },
+    JWT_SECRET,
+    { expiresIn: '1h' } 
+  );
+};
 
 async function main() {
-  const hashedPassword = await bcrypt.hash('admin@123', 10); 
+  const hashedPassword = await bcrypt.hash('admin@1234', 10);
 
   const admin = await prisma.admin.upsert({
-    where: { emailAddress: 'admin@gmail.com' }, 
-    update: {}, 
+    where: { emailAddress: 'admin@yahoo.com' },
+    update: {},
     create: {
-    username : 'admin',
-      emailAddress: 'admin@gmail.com', 
+      username: 'admin',
+      emailAddress: 'admin@yahoo.com',
       password: hashedPassword,
     },
   });
 
+
+  const token = generateToken(admin.id, admin.emailAddress);
+
   console.log('Admin user seeded:', admin);
+  console.log('JWT Token:', token);
 }
 
 main()
